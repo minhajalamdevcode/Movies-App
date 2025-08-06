1b32570c5929b8c7a6b4a1b200e3d576293fb1c4
minhajalamdevcode <myminhajalam392@gmail.com>
2025-08-06 20:11:19 +0530

    fixed responsive issue

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Header.css"
 import { Link } from "react-router-dom"
+import { FaBars } from "react-icons/fa"
+import { IoMdClose } from "react-icons/io"
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://files.readme.io/29c6fee-blue_short.svg" /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+                <Link to="/">
+                    <img className="header__icon" src="https://files.readme.io/29c6fee-blue_short.svg" alt="logo" />
+                </Link>
+                <div className="navLinks">
+                    <Link to="/movies/popular"><span>Popular</span></Link>
+                    <Link to="/movies/top_rated"><span>Top Rated</span></Link>
+                    <Link to="/movies/upcoming"><span>Upcoming</span></Link>
+                </div>
             </div>
+
+            <div className="mobileMenuIcon" onClick={toggleMenu}>
+                {menuOpen ? <IoMdClose /> : <FaBars />}
+            </div>
+
+            {/* Mobile Dropdown Menu */}
+            {menuOpen && (
+                <div className="mobileMenu">
+                    <Link to="/movies/popular" onClick={toggleMenu}><span>Popular</span></Link>
+                    <Link to="/movies/top_rated" onClick={toggleMenu}><span>Top Rated</span></Link>
+                    <Link to="/movies/upcoming" onClick={toggleMenu}><span>Upcoming</span></Link>
+                </div>
+            )}
         </div>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,33 +19,38 @@ const Home = () => {
         <>
             <div className="poster">
                 <Carousel
-                    showThumbs={false}
-                    autoPlay={true}
-                    transitionTime={3}
-                    infiniteLoop={true}
-                    showStatus={false}
-                >
-                    {
-                        popularMovies.map(movie => (
-                            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
-                                <div className="posterImage">
-                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
-                                </div>
-                                <div className="posterImage__overlay">
-                                    <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
-                                    <div className="posterImage__runtime">
-                                        {movie ? movie.release_date : ""}
-                                        <span className="posterImage__rating">
-                                            {movie ? movie.vote_average :""}
-                                            <i className="fas fa-star star-color" />{" "}
-                                        </span>
-                                    </div>
-                                    <div className="posterImage__description">{movie ? movie.overview : ""}</div>
-                                </div>
-                            </Link>
-                        ))
-                    }
-                </Carousel>
+    showThumbs={false}
+    autoPlay={true}
+    interval={3000}
+    infiniteLoop={true}
+    showStatus={false}
+    transitionTime={1000}
+    stopOnHover={false}
+    swipeable={true}
+    emulateTouch={true}
+>
+    {
+        popularMovies.map(movie => (
+            <Link key={movie.id} style={{ textDecoration: "none", color: "white" }} to={`/movie/${movie.id}`}>
+                <div className="posterImage">
+                    <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.original_title} />
+                </div>
+                <div className="posterImage__overlay">
+                    <div className="posterImage__title">{movie.original_title}</div>
+                    <div className="posterImage__runtime">
+                        {movie.release_date}
+                        <span className="posterImage__rating">
+                            {movie.vote_average}
+                            <i className="fas fa-star star-color" />
+                        </span>
+                    </div>
+                    <div className="posterImage__description">  {movie.overview?.split(" ").slice(0, 20).join(" ") + '...'}</div>
+                </div>
+            </Link>
+        ))
+    }
+</Carousel>
+
                 <MovieList />
             </div>
         </>
